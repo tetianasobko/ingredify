1369bbd602c46f63402ca6424b03cad107492952
Tetiana Sobko <sobkotiana@gmail.com>
2025-05-02 18:38:23 +0300

    Register models

--- ingredify/admin.py
+++ ingredify/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient
 
-# Register your models here.
+
+class IngredientInline(admin.TabularInline):
+    model = Ingredient
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("title", "difficulty", "cooking_time", "prep_time", "servings", "date_added")
+    search_fields = ("title", "ingredients__name")
+    list_filter = ("difficulty", "date_added")
+    inlines = [IngredientInline]
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ("name", "quantity", "unit", "recipe")
+    search_fields = ("name",)
+    list_filter = ("unit",)
